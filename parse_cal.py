666ee434a1379cfb2ac6fa293a8d9bcbdfdd64bd
Mattias Appelren <mattias@ppelgren.se>
2012-07-18 22:20:47 -0700

    filez

--- parse_cal.py
+++ parse_cal.py
@@ -19,33 +19,36 @@ def main():
     
     matrix.append(temprow)
      
-    #print zip(*matrix)[4]
     matrix = zip(*matrix)
     stops = matrix[1][1:]
     trains = matrix[2:-2]
+    del trains[22] # remove middle stop column
     trainList = []
 
     #print stops
     #print trains
 
     for train in trains:
-        #tTemp = {}
-        #tTemp['number'] = train[0]
-        #tTemp['stops'] = dict(zip(stops,train[1:]))
-        trainList.append(dict(number = train[0],
-                                stops = dict(zip(stops,train[1:]))
-        ))
-            
-    print '\n\n', trainList[0]    
+        trainList.append(dict(trainNumber = train[0],stopTimes = list(train[1:])))
+           
+    #print '\n\n', trainList[18]    
     #print trainList
 
     for train in trainList:
-        for (k, v) in train['stops'].iteritems():
-            if v != '-':
-                if train['number'] in ['146', '150', '154', '158', '260', '362',  '264', '266', '368', '270', '372', '274', '276', '378', '280', '382', '284', '386', '288', '190', '192', '194'] or (train['number'] == '142' & v.startswith('12')) or (train['number'] == '196' & !v.startswith('12')):
-                    train['stops'][k] += 'p'
+        for (k, v) in enumerate(train['stopTimes']):
+            if v.endswith('*'):
+                train['stopTimes'][k] = v[:-1]
+            if (v != '-' and 
+            (train['trainNumber'] in ['146', '150', '154', '158', '260', '362', '264', '266', '368', '270', '372', '274', '276', '378', '280', '382', '284', '386', '288', '190', '192', '194']
+            or ((train['trainNumber'] == '142' and v.startswith('12')) 
+            or (train['trainNumber'] == '196' and not v.startswith('12'))))
+            ):
+                    train['stopTimes'][k] += 'p'
        
-    print '\n\n', trainList[20]    
+    #print '\n\n', trainList[18]    
+    #print '\n\n', trainList[42]    
+    print 'trainList =', trainList, ';'
+    print 'stopList =', list(stops), ';'
 
 if __name__ == '__main__':
     main()
